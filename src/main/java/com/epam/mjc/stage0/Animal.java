735a4398d8f3478f1d6916124d92397b9cddf2fd
wickerman2022 <sverige@i.ua>
2023-08-03 22:23:31 +0300

    Try to solve

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,18 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription(){
+            String furStatus = hasFur ? "a" : "no";
+            String pawStr = (numberOfPaws == 1) ? "paw" : "paws";
+            return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + pawStr + " and "+ furStatus + " fur.";
+        };
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
 }
